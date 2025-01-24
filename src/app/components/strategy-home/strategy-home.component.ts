278d5c4bf0e9263de1f5e31aca7621dcc6896987
Abhishek Kumar <abhishek@Abhisheks-MacBook-Pro.local>
2025-01-24 14:37:28 +0530

    fix merger issue

--- src/app/components/strategy-home/strategy-home.component.ts
+++ src/app/components/strategy-home/strategy-home.component.ts
@@ -12,13 +12,15 @@ import { API_ENDPOINTS } from 'src/app/utils/api-constants';
 export class StrategyHomeComponent implements OnInit {
   dateList: { key: string; displayValue: string }[] = [];
   selectedDate: string = '';
-  displayDate:string='Select Date';
+  displayDate: string = 'Select Date';
   tradeList: any;
-  orderDataList: any=[];
-  brokerList: any=[];
+  orderDataList: any = [];
+  brokerList: any = [];
   selected_broker: any;
   tradeMap: Map<string, any> = new Map<string, any>();
-  selectedTrades:any=[];
+  selectedTrades: any = [];
+  multiplierArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  selectedMultiplyer:number=1;
   constructor(
     private http: HttpService,
     private loader: LoaderService,
@@ -30,7 +32,7 @@ export class StrategyHomeComponent implements OnInit {
   }
   getDateList() {
     this.loader.showLoader();
-    this.http.get(API_ENDPOINTS.STRAGEGY.DATE).subscribe((res:any )=> {
+    this.http.get(API_ENDPOINTS.STRAGEGY.DATE).subscribe((res: any) => {
       this.dateList = this.preparedDateDropDown(res.result);
       this.loader.hideLoader();
     })
@@ -42,8 +44,8 @@ export class StrategyHomeComponent implements OnInit {
     }));
   }
   onDateChange(obj: any) {
-    this.selectedDate=obj.key;
-    this.displayDate=obj.displayValue;
+    this.selectedDate = obj.key;
+    this.displayDate = obj.displayValue;
     if (this.selectedDate == '') {
       this.toaster.showError('Please Select Validate Date!!', "Error!");
       return;
@@ -52,30 +54,41 @@ export class StrategyHomeComponent implements OnInit {
   }
   getTradeList(selectedDate: string) {
     this.loader.showLoader();
-    this.http.get(API_ENDPOINTS.STRAGEGY.GET_BY_DATE(selectedDate)).subscribe((res:any) => {
-      this.tradeList=res.result;
+    this.http.get(API_ENDPOINTS.STRAGEGY.GET_BY_DATE(selectedDate)).subscribe((res: any) => {
+      this.tradeList = res.result;
       this.orderDataList = this.tradeList.order_data_list;
-      this.brokerList=this.tradeList.brokers;
-     // this.loader.hideLoader();
+      this.brokerList = this.tradeList.brokers;
+      // this.loader.hideLoader();
     })
   }
-  onBrokerChange(broker:any){
-    this.selected_broker=broker
+  onBrokerChange(broker: any) {
+    this.selected_broker = broker
 
   }
-  selectTrade(data:any){
-    data.status='B';
+  selectTrade(data: any) {
+    debugger
+    this.setValueOnSelected(data);
     if (this.tradeMap.has(data.trading_symbol)) {
       this.tradeMap.set(data.trading_symbol, { ...this.tradeMap.get(data.trading_symbol), ...data });
     } else {
       this.tradeMap.set(data.trading_symbol, data);
     }
-    this.selectedTrades=Array.from(this.tradeMap.values());
+    this.selectedTrades = Array.from(this.tradeMap.values());
   }
-  deleteTrade(data:any){
-    if(this.tradeMap.has(data.trading_symbol)){
+  setValueOnSelected(data: any): any {
+    data.status = 'B';
+    data.multiplier = 1;
+  }
+  deleteTrade(data: any) {
+    if (this.tradeMap.has(data.trading_symbol)) {
       this.tradeMap.delete(data.trading_symbol);
-      this.selectedTrades=Array.from(this.tradeMap.values());
+      this.selectedTrades = Array.from(this.tradeMap.values());
     }
+  }
+  updateQty(multiplier: any) {
+    this.selectedMultiplyer=multiplier;
+    this.selectedTrades = this.selectedTrades.map((trade:any)=> {
+      return { ...trade, multiplier: multiplier }; 
+    });
   }
 }
