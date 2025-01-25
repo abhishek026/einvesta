6c35fd95c9fb558debb2fbf0ad0c3261b33f4016
webdev-akshay <webdev.akshay@gmail.com>
2025-01-25 23:46:43 +0530

    ui changes

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -9,50 +9,55 @@ export class SidenavComponent implements OnInit {
   @Input() isExpanded: boolean = false;
   @Output() toggleSidebar = new EventEmitter<void>();
 
-
-  menuOpen = false;
-  isProfileVisible = false;
-  isNotificationVisible = false;
-  contentWrapperMid = false;
-
-  constructor() { }
-
+  menuOpen = false; // Controls `bar1` and `bar2`
+  showBar1 = true; // Controls `bar3`
+  showBar2=false;
+  showBar3=false;
+  showBreadCrumb=false
   menuItems = [
     {
       name: 'Strategy',
       tooltip: 'Programs',
       icon: 'fa fa-table',
-      path:'/strategy',
+      path: '/strategy',
       active: false,
       submenu: ['Program No 1', 'Program No 2', 'Program No 3', 'Program No 4'],
     },
     {
       name: 'Order History',
-      path:'/order-history',
+      path: '/order-history',
       tooltip: 'Companies',
       icon: 'fa fa-building',
-      active: false
-     },
-     {
+      active: false,
+    },
+    {
       name: 'Strategy Home',
-      path:'/strategy-home',
+      path: '/strategy-home',
       tooltip: 'Companies',
       icon: 'fa fa-building',
-      active: false
-     },
-
+      active: false,
+    },
   ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+      this.showBar3 = true;
+      this.showBar1=false;
+      this.showBreadCrumb=true // Show `bar3` when `menuOpen` is true
+    } else {
+      this.showBar3 = false;
+      this.showBreadCrumb=true;
+      this.showBar1=true; // Hide `bar3` when `menuOpen` is false
+    }
     this.toggleSidebar.emit();
-
   }
+
   toggleMenuItem(item: any): void {
     item.active = !item.active;
   }
-  ngOnInit(): void {
-  }
-
 }
