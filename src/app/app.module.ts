721bc2e302659d7d00c517905c4957cf1e59b328
Abhishek Kumar <abhishek@192.168.1.26>
2025-01-26 23:28:07 +0530

    change job time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ToastrModule } from 'ngx-toastr';  // Import ToastrModule from ngx-toastr
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
