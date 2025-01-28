d38b97baf69751c9f50de988abbce2b77600f573
Abhishek Kumar <abhishek@Abhisheks-MacBook-Pro.local>
2025-01-28 15:07:35 +0530

    login changes

---
+++ src/app/interceptors/HttpErrorInterceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ToastService } from '../services/toast.service';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+    constructor(private toaster:ToastService){
+
+    }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.toaster.showError('An error occurred while processing the request.')
+        return throwError(() => new Error('An error occurred while processing the request.'));
+      })
+    );
+  }
+}
--- src/app/utils/api-constants.ts
+++ src/app/utils/api-constants.ts
@@ -3,6 +3,7 @@ export const API_ENDPOINTS = {
     STRAGEGY: {
       DATE: `${environment.apiBaseUrl}/api/expiry-dates`,
       GET_BY_DATE: (date: string) => `${environment.apiBaseUrl}/api/expiry-data?expiry=${date}`,
+      SAVE_ORDER_TEMPLATE:`${environment.apiBaseUrl}/api/save-order-template`,
     }
   };
   
