fa263c5f780b013cd9833a65a200cd8c154af93b
Abhishek Kumar <abhishek@Abhisheks-MacBook-Pro.local>
2025-01-30 14:30:41 +0530

    fix issue

--- src/app/components/orderhistory/orderhistory.component.ts
+++ src/app/components/orderhistory/orderhistory.component.ts
@@ -14,6 +14,8 @@ export class OrderhistoryComponent implements OnInit {
   dbTemplateList: any = [];
   templateData: any;
   searchValue: string = '';
+  templateDataMap: Map<string, any[]> = new Map<string, any[]>();
+  templateDataList: any = [];
   constructor(private http: HttpService,
     private toaster: ToastService) { }
 
@@ -25,6 +27,7 @@ export class OrderhistoryComponent implements OnInit {
       this.templateData = res.result;
       this.templateList = this.preparedTemplateList(this.templateData);
       this.dbTemplateList = [...this.templateList];
+      this.templateDataMap = this.convertJsonToMap(this.templateData);
     })
   }
   preparedTemplateList(res: any): { template_name: string; created_date: string; }[] {
@@ -51,5 +54,21 @@ export class OrderhistoryComponent implements OnInit {
     this.templateList = this.templateList.filter((template: any) =>
       template.template_name.toLowerCase().includes(this.searchValue.toLowerCase())
     );
+  }
+  convertJsonToMap = (json: any): Map<string, any[]> => {
+    const map = new Map<string, any[]>();
+    Object.keys(json).forEach((key) => {
+      if (Array.isArray(json[key])) {
+        map.set(key, json[key]);
+      }
+    });
+    return map;
+  };
+  viewTemplateData(template_name: any) {
+    debugger
+    this.templateDataList = [];
+    if (this.templateDataMap.has(template_name)) {
+      this.templateDataList = this.templateDataMap.get(template_name);
+    }
   }
 }
