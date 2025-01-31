4e780d2e150e08bd3ba0af1684803354074ebb65
Abhishek Kumar <abhishek@Abhisheks-MacBook-Pro.local>
2025-01-31 09:50:18 +0530

    build changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/einvesta'),
+      dir: require('path').join(__dirname, './coverage/calculative'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'einvesta'`, () => {
+  it(`should have as title 'calculative'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('einvesta');
+    expect(app.title).toEqual('calculative');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('einvesta app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('calculative app is running!');
   });
 });
